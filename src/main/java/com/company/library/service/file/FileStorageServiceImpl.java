ea0a5f55d94fca9f52e5501c5ace93b77740ab90
jahongir <atametov97@mail.ru>
2022-11-10 00:09:22 +0500

    FileStorageServiceImpl class is added

--- src/main/java/com/company/library/service/file/FileStorageServiceImpl.java
+++ src/main/java/com/company/library/service/file/FileStorageServiceImpl.java
@@ -1,7 +1,32 @@
 package com.company.library.service.file;
 
-public class FileStorageServiceImpl {
+import com.company.library.configs.ApplicationContextHolder;
+import com.company.library.dao.AbstractDAO;
+import com.company.library.dao.UploadDAO;
+import com.company.library.domains.Uploads;
+import com.company.library.dto.UploadsDTO;
+import com.company.library.exceptions.NotFoundException;
+import lombok.NonNull;
+
+import javax.servlet.http.Part;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+import java.util.Optional;
+
+public class FileStorageServiceImpl extends AbstractDAO<UploadDAO> implements FileStorageService {
     private static FileStorageServiceImpl fileStorageService;
+
+    private Path rootPath = Paths.get("/home/jahongir/Documents/upload");
+
+    public FileStorageServiceImpl() {
+        super(ApplicationContextHolder.getBean(UploadDAO.class));
+    }
+
 
     public static FileStorageServiceImpl getInstance() {
 
@@ -9,5 +34,84 @@ public class FileStorageServiceImpl {
             fileStorageService = new FileStorageServiceImpl();
         }
         return fileStorageService;
+    }
+
+    @Override
+    public Uploads upload(Part part) {
+        try {
+            String contentType = part.getContentType();
+            String originalFileName = part.getSubmittedFileName();
+
+            originalFileName = originalFileName.replaceAll(",", "_");
+            long size = part.getSize();
+
+            String[] split = originalFileName.split("\\.");
+
+            String fileNameExtension = split[split.length - 1];
+
+            String generatedName = System.currentTimeMillis() + "." + fileNameExtension;
+
+            String path = "/upload/" + generatedName;
+
+            Uploads uploads = Uploads.builder()
+                    .contentType(contentType)
+                    .originalName(originalFileName)
+                    .generatedName(generatedName)
+                    .size(size)
+                    .path(path)
+                    .build();
+
+            Path uploadPath = rootPath.resolve(generatedName);
+
+            dao.save(uploads);
+
+            Files.copy(part.getInputStream(), uploadPath, StandardCopyOption.REPLACE_EXISTING);
+            return uploads;
+
+        } catch (IOException e) {
+            throw new RuntimeException("Something wrong try again");
+        }
+
+    }
+
+    @Override
+    public File download(@NonNull String fileName) {
+        return null;
+    }
+
+    @Override
+    public UploadsDTO get(@NonNull String fileName) {
+        return null;
+    }
+
+    @Override
+    public List<Uploads> getAll() {
+        return null;
+    }
+
+    @Override
+    public UploadsDTO getByPath(String path) throws NotFoundException {
+
+        Optional<Uploads> byPath = dao.getByPath(path);
+
+        if (byPath.isEmpty()) {
+            throw new NotFoundException("File not found");
+        }
+
+        Uploads uploads = byPath.get();
+
+        return UploadsDTO.builder()
+                .id(uploads.getId())
+                .path(uploads.getPath())
+                .generatedName(uploads.getGeneratedName())
+                .size(uploads.getSize())
+                .contentType(uploads.getContentType())
+                .originalName(uploads.getOriginalName())
+                .build();
+    }
+
+    @Override
+    public Uploads extractCover(Part book) {
+        return null;
     }
 }
